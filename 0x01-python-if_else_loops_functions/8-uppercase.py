2ba906247bbfbc19fa9074882164d0726a428cf9
Femi Mehalayese <megafemgroup@gmail.com>
2024-03-13 15:10:03 +0100

    upperchange

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for char in str:
-        i = ord(char)
         if i <= 96 or i >= 123:
-            print("{:c}".format(i), end="")
+            result += char
         else:
-            print("{:c}".format(i - 32), end="")
+            result += char
+    print("{}".format(result))
