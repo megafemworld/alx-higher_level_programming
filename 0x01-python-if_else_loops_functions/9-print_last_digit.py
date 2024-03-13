e335ee8b54d16a100245d958ddc9786ab7732d72
Femi Mehalayese <megafemgroup@gmail.com>
2024-03-13 15:36:35 +0100

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,6 +2,8 @@
 def print_last_digit(number):
     if number < 0:
         result = number % -10
+        print(result)
     else:
         result = number % 10
+        print(result)
     return (result)
