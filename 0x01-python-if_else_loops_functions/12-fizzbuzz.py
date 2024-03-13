cf64a5436c1bf8102c8815920787a5a8beabf68b
Femi Mehalayese <megafemgroup@gmail.com>
2024-03-13 16:50:36 +0100

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,12 +3,12 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
             if i % 5 == 0:
-                print("{}".format("fizzBuzz"), end=" ")
+                print("{}".format("FizzBuzz"), end=" ")
             else:
                 print("{}".format("Fizz"), end=" ")
         elif i % 5 == 0:
             if i % 3 == 0:
-                print("{}".format("fizzBuzz"), end=" ")
+                print("{}".format("FizzBuzz"), end=" ")
             else:
                 print("{}".format("Buzz"), end=" ")
         else:
