a8caa8d6a65f3e99603a02b956e8ce041e0d58f2
Femi Mehalayese <megafemgroup@gmail.com>
2024-03-14 20:47:08 +0100

    arg printer

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+   num_arg = len(sys.argv) - 1
+   i = 1
+   if num_arg < 1:
+       print("{} argument.".format(num_arg))
+   else:
+        print("{} arguments:".format(num_arg))
+        for arg in sys.argv[1:]:
+            print("{}: {}".format(i, arg))
+            i += 1 
