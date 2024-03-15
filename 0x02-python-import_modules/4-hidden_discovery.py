5f1add81a50a050e15ecb35dadaefd565a64094c
Femi Mehalayese <megafemgroup@gmail.com>
2024-03-15 05:33:05 +0100

    _ex

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-import sys
 import hidden_4
 if __name__ == "__main__":
-    for dir in dir():
-        if dir[0] == "_":
-            continue
-        else:
-            print("{}".format(dir)
+    for d in dir(hidden_4):
+        if d[0] != "__":
+            print("{}".format(d))
