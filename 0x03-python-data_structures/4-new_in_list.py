37e0b3e49e8fdbbb092dcbf497a3fee6460e2d5d
Femi mehalayese <megafemgroup@gmail.com>
2024-03-16 19:38:16 +0100

    check c

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     n_list = my_list[:]
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return n_list
     else:
         n_list[idx] = element
