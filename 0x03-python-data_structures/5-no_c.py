a7bdc9c4fafac62d0670e262a0e1c004e3451dca
Femi Mehalayese <megafemgroup@gmail.com>
2024-03-17 23:58:13 +0100

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,7 +6,7 @@ def no_c(my_string):
         new_string.append(i)
     for j in range(len(new_string)):
         if new_string[j] == 'C' or new_string[j] == 'c':
-            new_string.pop([j])
+            new_string.pop(j)
         else:
             continue
     for k in new_string:
