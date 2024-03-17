d473e48c647bc3e5246d6e561ea5743edd89d64a
Femi Mehalayese <megafemgroup@gmail.com>
2024-03-17 07:10:55 +0100

    rev correction

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     j = len(my_list) - 1
-    for j in my_list:
+    for i in my_list:
         print("{:d}".format(my_list[j]))
         j -= 1
