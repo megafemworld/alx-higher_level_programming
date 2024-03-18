fed818e876718c54cda6f8e22e7b136f8f91f90b
Femi Mehalayese <megafemgroup@gmail.com>
2024-03-18 12:47:40 +0100

    matrix integr

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) <= 0:
-        print("{}".format("$"))
+    if len(matrix) <= 1:
+        print()
     else:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if len(matrix[i]) - 1 == j:
-                    print("{:d}$".format(matrix[i][j]))
+                    print("{:d}".format(matrix[i][j]))
                 else:
                     print("{:d}".format(matrix[i][j]), end=" ")
