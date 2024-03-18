b0447e8153a4a769f21154bd7c26652bfc25338b
Femi Mehalayese <megafemgroup@gmail.com>
2024-03-18 02:02:21 +0100

    2d tf

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     divisible_by_2 = []
     for i in my_list:
         if i % 2 == 0:
-            divisible_by_2.append(True)
+            divisible_by_2.append('True')
         else:
-            divisible_by_2(False)
+            divisible_by_2('False')
     return divisible_by_2
