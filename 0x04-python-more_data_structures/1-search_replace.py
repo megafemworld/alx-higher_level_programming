8bc6ae89206613e5f1fc0204601f10703b03cd9f
Femi mehalayese <megafemgroup@gmail.com>
2024-03-20 22:16:35 +0100

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    rep_list = my_list[:]
     if my_list == []:
         return
     else:
-        for i in range(len(my_list)):
-            if my_list[i] == search:
-                my_list[i] = replace
+        for i in range(len(rep_list)):
+            if rep_list[i] == search:
+                rep_list[i] = replace
             else:
                 continue
-    return my_list
+    return rep_list
