abf1fd1111fbf65b3c90e1e6a9c9f163e4870df8
Femi mehalayese <megafemgroup@gmail.com>
2024-03-20 23:17:31 +0100

    print sorted

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary == {}:
+        return
+    else:
+        for i, j in sorted(a_dictionary.items()):
+            print("{} : {}".format(i, j))
