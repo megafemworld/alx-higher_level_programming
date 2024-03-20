6eacf4eb13960e9cf47207adae9286419d324182
Femi mehalayese <megafemgroup@gmail.com>
2024-03-21 00:29:06 +0100

    mulx2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+new_dic = {}
 def multiply_by_2(a_dictionary):
     for x, y in a_dictionary.items():
-        new_dict[x] = y*2
-    return new_dict
+        new_dic[x] = y*2
+    return new_dic
