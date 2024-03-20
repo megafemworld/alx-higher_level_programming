f14060abb3628b9c57f07b40cdcf033f5cf33b65
Femi mehalayese <megafemgroup@gmail.com>
2024-03-21 00:05:21 +0100

    delete by key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -4,7 +4,7 @@ def simple_delete(a_dictionary, key=""):
         return a_dictionary
     else:
         if key in a_dictionary:
-            del( a_dictionary[key])
+            del a_dictionary[key]
         else:
             return a_dictionary
     return a_dictionary
