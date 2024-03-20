e2b14f1988cfeff234fef5b690a4571ce20cf3e2
Femi mehalayese <megafemgroup@gmail.com>
2024-03-20 22:37:52 +0100

    common list

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    common = []
+    if set_1 == {} or set_2 == {}:
+        return common
+    else:
+        common = list(set1 & set_2)
+    return common
