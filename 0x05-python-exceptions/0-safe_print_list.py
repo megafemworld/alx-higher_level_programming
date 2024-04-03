559deaed4e68f478bed6b29b0cc2e6942fbac5e9
Femi mehalayese <megafemgroup@gmail.com>
2024-04-03 15:43:22 +0100

    print x

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    counter = 0
     try:
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
-        print()
-        return x
-    except:
-        counter = 0
-        for i in my_list:
-            print("{}".format(i), end="")
             counter += 1
+        print()
+        return counter
+    except:
         print()
         return counter
