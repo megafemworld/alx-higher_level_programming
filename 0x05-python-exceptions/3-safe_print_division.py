34cf3e59d22004d0af571c0e604c69ecf8c979ed
Femi mehalayese <megafemgroup@gmail.com>
2024-04-03 23:12:01 +0100

    print division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except Expection as e:
+    except Exception as e:
         result = None
     finally:
         print("Inside result: {}".format(result))
