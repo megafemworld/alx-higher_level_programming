533e180870ae71f440ccef9d2ebefeb269b562cb
Femi mehalayese <megafemgroup@gmail.com>
2024-04-03 15:14:21 +0100

    print x

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x > 0
+    if x > 0:
         try:
             for i in range(x):
                 print("{}".format(my_list[i]), end="")
