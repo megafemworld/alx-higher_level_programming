fadadcac48727db0d03a0f4ce79e4a2ec8b7327b
Femi mehalayese <megafemgroup@gmail.com>
2024-04-03 21:39:46 +0100

    print integer & skip text

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,15 @@
 def safe_print_list_integers(my_list=[], x=0):
     counter = 0
     try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i], end=""))
+        for i in my_list:
+            num = my_list[i]
+            if isinstance(num, int):
+                print("{:d}".format(i), end="")
                 counter += 1
             else:
                 continue
         print()
+        return counter
     except Exception as e:
         print()
         return counter
