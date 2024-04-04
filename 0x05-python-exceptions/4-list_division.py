2148ff409710301f45675a33a6f2fec417ea439c
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-04 12:55:49 +0100

    divider

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    div_list = []
+    try:
+        for i in range(list_length):
+            if my_list_1[i] == 0 or my_list_2[i] == 0:
+                div_list.append(0)
+                print("division by 0")
+            elif type(my_list_1[i]) != int or type(my_list_2[i]) != int:
+                div_list.append(0)
+                print("wrong type")
+            else:
+                div_list.append(my_list_1[i]/my_list_2[i])
+        return div_list
+    except Exception as e:
+        div_list.append(0)
+        print("out of range")
+    finally:
+        print("{}".format(div_list))
