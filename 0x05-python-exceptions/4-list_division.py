4674dd6dba7b93f7d9dab5ce1d26ffa7fa1626ea
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-04 13:02:34 +0100

    divider

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
             if my_list_1[i] == 0 or my_list_2[i] == 0:
                 div_list.append(0)
                 print("division by 0")
-            elif type(my_list_1[i]) != int or type(my_list_2[i]) != int:
+            elif type(my_list_1[i]) is str or type(my_list_2[i]) is str:
                 div_list.append(0)
                 print("wrong type")
             else:
