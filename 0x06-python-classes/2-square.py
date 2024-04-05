0db2ee00b1a017faf10ed17fbd6c90abf8292abf
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-05 15:47:31 +0100

    attr checker

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Square class defination """
+
+
+class Square:
+    """ private Size attribute defination
+        When size is not an integer show TypeError
+        When Size is less than 0 show ValueError
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
