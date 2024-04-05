58ac97e8405b075ce7629e37ebed1d6f3c182364
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-05 12:56:41 +0100

    class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """
-    Represents a square.
-
-    This class defines a square with no methods or attributes.
-    """
+    """This class defines a square with no methods or attributes."""
     pass
