af17e16e1ef3031ea91dbd48d42930983a2ee4d6
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-06 19:00:10 +0100

    sqaure setter & getter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,12 +8,7 @@ class Square:
         When Size is less than 0 show ValueError
     """
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.value = size
 
     @property
     def size(self):
