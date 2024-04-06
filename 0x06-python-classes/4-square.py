a76629fed8ce913360ab1a68d4017a92fa67c92f
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-06 19:18:02 +0100

    sqaure setter & getter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,11 +7,7 @@ class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
-        """
+        """Initialize a new square."""
         self.size = size
 
     @property
