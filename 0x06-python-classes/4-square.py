2aa75000c678367b5f641b340d3d8dc45c8362f5
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-06 19:26:45 +0100

    sqaure setter & getter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-# 4-square.py
-"""Define a class Square."""
+""" Square class defination """
 
 
 class Square:
-    """Represent a square."""
-
+    """ private Size attribute defination
+        When size is not an integer show TypeError
+        When Size is less than 0 show ValueError
+    """
     def __init__(self, size=0):
-        """Initialize a new square."""
+        """Initialize a new square"""
         self.size = size
 
     @property
     def size(self):
-        return (self.__size)
+        """Retrieve size."""
+        return self.__size
 
     @size.setter
-    def size(self, value):
-        if type(value) is not int:
+    def x(self, value):
+        if type(valueare) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     def area(self):
+        """Return the area of the square."""
         area = self.__size * self.__size
         return area
