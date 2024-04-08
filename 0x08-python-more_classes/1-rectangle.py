debf4b49a5ad5c0b367b4ab453287fc317a31fa4
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-08 17:10:05 +0100

    empty

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Rectangle class defination"""
+
+
+class Rectangle:
+    """Represent a rectangle"""
+   def  __init__(self, width=0, height=0):
+       self.width = width
+       self.height = height
+    @property
+    def width
