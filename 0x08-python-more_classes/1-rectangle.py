8243ff703c99d44157f5d598cfed0389d63de432
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-11 14:42:31 +0100

    rectngle more

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,8 +4,32 @@
 
 class Rectangle:
     """Represent a rectangle"""
-   def  __init__(self, width=0, height=0):
-       self.width = width
-       self.height = height
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
     @property
-    def width
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(height):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(int, value):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
