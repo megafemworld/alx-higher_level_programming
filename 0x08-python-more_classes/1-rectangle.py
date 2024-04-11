3ee24c22b376908736dee4f551ce3619fc7cae9f
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-11 14:49:15 +0100

    height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if not isinstance(int, value):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
