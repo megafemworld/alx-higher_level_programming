30b7b94d98cada1620b1003bbf6f68d748178e28
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-15 17:11:02 +0100

    qqqqqqqqqqqqqqqqqqqqqxxxxxxxxxxxxdddddddddddddd

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""Module 4-inherits_from.
-Finds if the object is an instance of a class that inherited
-(directly or indirectly) from the specified class."""
+
+"""
+File: 4-inherits_from.py
+Desc: This file contains a single function defination.
+Author: Gizachew Bayness (Elec Crazy).
+Date Created: Aug 1 2022
+"""
 
 
 def inherits_from(obj, a_class):
-    """Determines if obj is an instance of a class that
-    inherited from a_class.
-
-    Args:
-        - obj: object to look at
-        - a_class: class to evaluate
-
-    Returns: True or False
+    """
+    This function  returns True if the object is an
+    instance of a class that inherited (directly or indirectly)
+    from the specified class ; otherwise False.
     """
 
-    return isinstance(obj, a_class) and type(obj) != a_class
+    if isinstance(obj, a_class) and type(obj) != a_class:
+        return True
+    return False
