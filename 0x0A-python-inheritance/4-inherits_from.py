de009192d224d7292ff9ca8034631ba669b10163
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-15 16:32:24 +0100

    inherits from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" inherits_from function
+==================================
+How to use inherits_from function
+=================================
+return True if object is an instance
+Otherwise False
+obj - Object to check
+a_class - class that want to check through
+"""
+
+
+def inherits_from(obj, a_class):
+    """ is_same_class function
+==================================
+How to use inherits_from function
+=================================
+return True if object found
+Otherwise False
+Args:
+obj - Object to check
+a_class - class that want to check through
+"""
+    if issubclass(obj, a_class):
+        return True
+    else:
+        return False
