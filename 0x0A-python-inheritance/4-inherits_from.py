e09c7bfb6fe977f3381fc2f4e24e8a565312063a
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-15 16:49:04 +0100

    add another if to inherit from function

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -21,8 +21,9 @@ Args:
 obj - Object to check
 a_class - class that want to check through
 """
-    if issubclass(obj, a_class) or
-    isinstance(obj, a_class) and type(obj) != a_class:
+    if issubclass(obj, a_class):
+        return True
+    if isinstance(obj, a_class) and type(obj) != a_class:
         return True
     else:
         return False
