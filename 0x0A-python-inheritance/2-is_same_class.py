14ef410e2d9dd66f33c7ef72a0f5121083100bb7
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-15 16:10:45 +0100

    correct is same class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -11,6 +11,16 @@ a_class - class that want to check through
 
 
 def is_same_class(obj, a_class):
+    """ is_same_class function
+==================================
+How to use is_same_class function
+=================================
+return True if object found
+Otherwise False
+Args:
+obj - Object to check
+a_class - class that want to check through
+"""
     if isinstance(obj, a_class):
         return True
     else:
