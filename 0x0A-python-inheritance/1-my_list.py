c0eabd9c41dd3eed3c7981e61e5116569a26eb1a
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-15 15:30:28 +0100

    sort & print

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,7 @@ class Mylist(list):
         """print_sorted - sort all element in the list and print the result out
             self: get the list from the Base class
         """
+        pass
         for i in range(len(self.my_list)):
             for j in range(1, len(self.my_list)):
                 if self.my_list[i] > self.my_list[j]:
