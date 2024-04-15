ca4604ea87d6c22c8e2152fa70824bbb642826c2
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-15 15:28:10 +0100

    list sort and print

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Mylist subclass defination"""
+
+
+class Mylist(list):
+    def print_sorted(self):
+        """print_sorted - sort all element in the list and print the result out
+            self: get the list from the Base class
+        """
+        for i in range(len(self.my_list)):
+            for j in range(1, len(self.my_list)):
+                if self.my_list[i] > self.my_list[j]:
+                    self.my_list[i] = self.my_list[i] ^ self.my_list[j]
+                    self.my_list[j] = self.my_list[i] ^ self.my_list[j]
+                    self.my_list[i] = self.my_list[i] ^ self.my_list[j]
+                else:
+                    continue
+        print(self.my_list)
