0e028001f20669337311316a005877ac19c20ffd
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-15 15:33:46 +0100

    sort & print

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""Mylist subclass defination"""
+"""
+===========================
+Module with class MyList
+===========================
+"""
 
 
-class Mylist(list):
+class MyList(list):
+    """Class with method print_sorted"""
+    pass
+
     def print_sorted(self):
-        """print_sorted - sort all element in the list and print the result out
-            self: get the list from the Base class
-        """
-        pass
-        for i in range(len(self.list)):
-            for j in range(1, len(self.list)):
-                if self.list[i] > self.list[j]:
-                    self.list[i] = self.list[i] ^ self.list[j]
-                    self.list[j] = self.list[i] ^ self.list[j]
-                    self.list[i] = self.list[i] ^ self.list[j]
-                else:
-                    continue
-        print(self.my_list)
+        """Methot that sorted a list"""
+
+        print(sorted(list(self)))
