9dc7f5988cca90d18b30e97216a485ab181330a9
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-15 17:31:39 +0100

    wudbcnmv

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -13,6 +13,6 @@ def inherits_from(obj, a_class):
     from the specified class ; otherwise False.
     """
 
-    if issubclass(obj, a_class) and type(obj) != a_class:
+    if isinstance(obj, a_class) and type(obj) != a_class:
         return True
     return False
