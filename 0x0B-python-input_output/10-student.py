4efb0ddd2117cbfde5180232f75d91fb3f8c801d
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-17 16:10:11 +0100

    filter json

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -2,16 +2,18 @@
 """ this is a module with a  student class"""
 
 
-class Student:
+class Student():
     """this is a student class"""
     def __init__(self, first_name, last_name, age):
+        """initialize defination"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """returns dictionary representation of Student"""
-        if attrs is None:
-            return self.__dict__
-        return {attr: getattr(self, attr) for attr in
-                attrs if hasattr(self, attr)}
+        if type(attrs) == list and all(type(i) == str for i in attrs):
+            return ({key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)})
+        return self.__dict__
+~                                
