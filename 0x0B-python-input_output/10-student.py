2aada1217913171f3e1a1228139635741f3036ae
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-17 14:44:39 +0100

    json filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Student defination """
+
+
+class Student():
+    """
+    A simple stundent class.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        This function initializes the object parameters.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        This function retrieves a dictionary representation
+        of a Student instance
+        """
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result_dict[attr] = getattr(self, attr)
+            return (result_dict)
