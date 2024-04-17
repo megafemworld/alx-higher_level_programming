0dcf018e67683b156d750c4aa13fcc4907855d52
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-17 16:16:34 +0100

    rfyghkjl

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" this is a module with a  student class"""
+"""Defines a class Student."""
 
 
 class Student():
-    """this is a student class"""
+    """Represent a student."""
     def __init__(self, first_name, last_name, age):
-        """initialize defination"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
