0baa187616633e93ad26d9f6f3f42bd94af45eb2
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-17 16:28:35 +0100

    dfgh

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-"""class Student"""
+"""
+A Student class that defines a Studen (module)
+"""
 
 
-class Student:
-    """class Student that defines a student
-    by: - first_name
-        - last_name
-        - age
+class Student():
     """
-
+        A Student class that defines a Student
+    """
     def __init__(self, first_name, last_name, age):
+        """
+            INIT
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        _list = {}
-        if attrs or attrs == []:
-            for i in attrs:
-                for key, value in self.__dict__.items():
-                    if key == i:
-                        _list[key] = value
-                        break
-        else:
-            return self.__dict__
-
-        return _list
+        """
+            retrieves a dictionary representation
+            of a Student instance
+        """
+        if type(attrs) is list and all([type(x) == str for x in attrs]):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return(self.__dict__)
