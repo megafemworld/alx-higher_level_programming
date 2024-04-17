dd9ee0f3ff07cdf1c60094cafc7d78247e06a772
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-17 13:11:46 +0100

    json class

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,9 +2,11 @@
 
 
 def class_to_json(obj):
-    """
+    """ class_to_json
     This function returns the dictionary description with
     simple data structure (list, dictionary, string, integer
     and boolean) for JSON serialization of an object
+    Arg:
+        obj - This is an instance of a class to return its json
     """
     return (obj.__dict__)
