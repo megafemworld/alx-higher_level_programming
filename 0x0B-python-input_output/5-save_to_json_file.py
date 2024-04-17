fac298510069741b6a3c010f38e36ccab27c4e52
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-17 10:38:22 +0100

    5c

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -16,4 +16,4 @@ def save_to_json_file(my_obj, filename):
     """
     import json
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(json.dump(obj, f))
+        f.write(json.dumps(obj))
