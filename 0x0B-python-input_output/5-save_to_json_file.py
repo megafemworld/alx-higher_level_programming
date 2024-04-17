28dd1c4ad98f7aaea6e94de0d3100f544df4370a
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-17 10:33:26 +0100

    correct 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -15,5 +15,6 @@ def save_to_json_file(my_obj, filename):
         my_obj: The object to serialize.
     """
 
-    with open(filename, "w") as f:
+    import json
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(json.dump(obj, f))
