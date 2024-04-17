fb851701e11ad733c4cacd32e89b4a7fa85d8e75
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-17 16:12:23 +0100

    filter json json

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -15,5 +15,4 @@ class Student():
         if type(attrs) == list and all(type(i) == str for i in attrs):
             return ({key: getattr(self, key)
                     for key in attrs if hasattr(self, key)})
-        return self.__dict__
-~                                
+        return self.__dict__  
