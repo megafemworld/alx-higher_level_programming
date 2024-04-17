0f91a8a24343f4d024315852822f0b267376cf59
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-17 11:13:50 +0100

    vyvyu

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Load, add, save
+# 7-add_item.py module
 ===================================================
  a script that adds all arguments to a Python list,
  and then save them to a file
