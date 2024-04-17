ebf5c4c3ca933489881149ea418a1b76a1ee268c
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-17 16:03:40 +0100

    10 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+# 10-student.py
+# Femi Mehalayese <megafemgroup0@gmail.com>
+"""Create a Class  Student"""
 
 
-class Student:
-    """Represent a student."""
+class Student(object):
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
+
+        """retrieve the dictionary representation of a Student instance"""
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+            return {key: getattr(self, key)
+                    for key in attr if hasattr(self, key)}
+
         return self.__dict__
