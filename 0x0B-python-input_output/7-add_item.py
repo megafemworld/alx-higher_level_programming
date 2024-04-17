5d534e788dfe7e75ee17f437c2c6721c16373081
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-17 12:54:54 +0100

    las

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,3 +7,17 @@
 ====================================================
 """
 import sys
+import os
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+if os.path.exists(filename):
+    my_obj = load_from_json_file(filename)
+else:
+    my_obj = []
+
+my_obj.extend(sys.argv[1:])
+save_to_json_file(my_obj, filename)
