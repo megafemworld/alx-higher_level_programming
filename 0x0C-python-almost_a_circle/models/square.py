2f6b16fa0fcd9bf1aac9fc3fce252d74f2e9e577
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-22 22:46:53 +0100

    square setter and getter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,5 +11,20 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return (f"[square] ({self.id})
+        return (f"[square] ({self.id}")
+    
+    @property
+    def size(self):
+        """Retrieve size of sqaure"""
+        return self.width
 
+    @size.setter
+    def size(self, value):
+        """assigns value to width and height"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.width = value
+            self.height = value
