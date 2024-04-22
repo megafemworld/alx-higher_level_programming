ec3500d2c84fba20166ad8c1a654243b3151cd1e
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-22 11:05:42 +0100

    say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,8 +5,9 @@
 
 def say_my_name(first_name, last_name=""):
     """This function takes in first_name mandatory and last_name option"""
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    elif not isinstance(last_name, str):
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
