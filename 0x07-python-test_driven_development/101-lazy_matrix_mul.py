d9ddfc687ed1d4bc37022bbcae33b722df55bcae
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-22 14:16:48 +0100

    matrix lazy

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import numpy as np
+"""multiplying matrixs module"""
+
+def lazy_matrix_mul(m_a, m_b):
+    """ function that multiplies 2 matrices by using the module NumPy"""
+    matrix_a = np.array(m_a)
+    matrix_b = np.array(m_b)
+
+    result = np.matmul(matrix_a, matrix_b)
+    return result
