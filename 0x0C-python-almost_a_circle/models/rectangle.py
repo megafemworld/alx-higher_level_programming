5717c62edb186c1b51d09c37f9c96f1b94bdb814
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-22 23:24:00 +0100

    rectangle kwarg

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -115,15 +115,15 @@ class Rectangle(Base):
     def update(self, *args, **kwargs):
         """ Update attributes """
         if args:
-            if len(args) >= 1:
+            if len(args) > 0:
                 self.__id = args[0]
-            if len(args) >= 2:
+            if len(args) > 1:
                 self.__width = args[1]
-            if len(args) >= 3:
+            if len(args) > 2:
                 self.__height = args[2]
-            if len(args) >= 4:
+            if len(args) > 3:
                 self.__x = args[3]
-            if len(args) >= 5:
+            if len(args) > 4:
                 self.__y = args[4]
         elif kwargs:
             for key, value in kwargs.items():
