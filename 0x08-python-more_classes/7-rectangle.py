281225ed7deb89b260eb0823b2bfc4a753638cf9
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-22 15:07:42 +0100

    print symbol

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -57,7 +57,7 @@ class Rectangle:
             return display
         else:
             for i in range(self.__height):
-                display += ("#" * self.__width)
+                display += (Rectangle.print_symbol * self.__width)
                 if i is not self.__height - 1:
                     display += "\n"
         return (display)
