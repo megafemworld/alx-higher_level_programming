1931b2ed741f8bbf74ef8452bac3c2e13954a9aa
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-22 12:07:37 +0100

    text indentaion

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,3 +4,16 @@
 
 
 def text_indentation(text):
+    """function that prints a text with 2 new lines after
+        each of these characters: ., ? and :"""
+
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    chars_split = ['.', '?', ':']
+    for char in text:
+        print(char, end='')
+        if char in chars_split:
+            print("\n\n", end='')
+    if text[-1] not in chars_split:
+        print()
