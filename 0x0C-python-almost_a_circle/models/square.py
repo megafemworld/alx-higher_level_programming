1048b930e1cb10c717179c4fdad63469607cc883
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-22 23:01:13 +0100

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-from models.rectangle import Rectangle
 """ Sqaure class, its attributes and methods """
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Initilize sqaure """
+    """ Sqaure class and methods """
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return (f"[square] ({self.id}")
+        """str representaion"""
+        return (f"[square] ({self.id}) {self.x}/{self.y} - {self.width}")
 
     @property
     def size(self):
