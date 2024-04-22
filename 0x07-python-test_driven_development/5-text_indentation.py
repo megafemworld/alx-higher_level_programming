cc4a8db1f0c70091a7d1c4f88fdcc1c1ad9f4a6b
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-22 12:33:25 +0100

    index characters

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-
-"""text indentation function"""
+"""module that prints a text with 2 new lines after each
+    of these characters: ., ? and :"""
 
 
 def text_indentation(text):
-    """function that prints a text with 2 new lines after
-        each of these characters: ., ? and :"""
-
+    """function that prints a text with 2 new lines
+    after each of these characters: ., ? and :"""
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    characters = [".", "?", ":"]
     count = 0
     while count < len(text) and text[count] == " ":
         count = count + 1
 
     while count < len(text):
         print(text[count], end="")
-        if text[count] == "\n" or text[count] in characters:
-            if text[count] in characters:
+        if text[count] == "\n" or text[count] in ".?:":
+            if text[count] in ".?:":
                 print("\n")
             count = count + 1
             while count < len(text) and text[count] == " ":
                 count = count + 1
             continue
-        count = count + 1`
+        count = count + 1
