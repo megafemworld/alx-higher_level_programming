e6403646ff95fbcfb4817c65672bdefa441700b4
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-23 00:34:32 +0100

    uodate args

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,15 +96,15 @@ class Rectangle(Base):
         """ Update attributes """
         if len(args) != 0 and args:
             if len(args) > 0:
-                self.__id = args[0]
+                self.id = args[0]
             if len(args) > 1:
-                self.__width = args[1]
+                self.width = args[1]
             if len(args) > 2:
-                self.__height = args[2]
+                self.height = args[2]
             if len(args) > 3:
-                self.__x = args[3]
+                self.x = args[3]
             if len(args) > 4:
-                self.__y = args[4]
+                self.y = args[4]
             return
         for key in kwargs:
             if key == "id":
