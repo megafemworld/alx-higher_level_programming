efa82fb87b04651c521f8a06d198b76bdf4669ea
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-22 09:46:38 +0100

    int add

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
-"""
-This is a function.
+""" adds two integers """
 
-
-This is a function, add_integer() that add two integer together. For example,
-
->>> add_integer(1, 2)
-3
-"""
 
 def add_integer(a, b=98):
-    """Return the addition of two integers."""
+    """ Returns the sum of two integers"""
 
-
-    if type(a) is not int and type(a) is not float:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    return (a + b)
+
+    return int(a) + int(b)
