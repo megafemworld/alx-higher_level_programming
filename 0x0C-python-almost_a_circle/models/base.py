46361416f20d02fd96309d0a0f7b23edcd260605
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-23 10:48:20 +0100

    base jump

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 """Base class """
 
 
@@ -15,3 +16,9 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if not list_dictionaries or list_dictionaries != []:
+            return ([])
+        return (json.dumps(list_dictionaries))
