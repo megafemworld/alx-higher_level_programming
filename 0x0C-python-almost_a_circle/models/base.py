f8824ff7a9d9184c91c6a127c33ee85d060d53b4
Femi Mehalayese <megafemgroup@gmail.com>
2024-04-23 10:50:10 +0100

    base jump

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,6 +19,7 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """Returns the JSON string representation"""
         if not list_dictionaries or list_dictionaries != []:
             return ([])
         return (json.dumps(list_dictionaries))
