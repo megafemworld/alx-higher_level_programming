065303c7d625c8514f2f42d4bf4af43afae7630e
Femi Mehalayese <megafemgroup@gmail.com>
2024-05-23 00:22:21 +0100

    create d, u, and p

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,11 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2
--- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+-- Create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if it does not exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on the database to the user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Apply the changes
 FLUSH PRIVILEGES;
