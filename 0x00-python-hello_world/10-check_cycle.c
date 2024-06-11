4c15a53c234b428b1c18279d8489d2d64daafe1d
Femi Mehalayese <megafemgroup@gmail.com>
2024-06-11 14:25:33 +0100

    cycle tester

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle.
+ * @list: the list to check.
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t temp;
+
+	if (list == NULL)
+		return (0);
+	temp = (*list)->next;
+	while (temp)
+	{
+		if (temp == NULL)
+			return (0);
+		if (temp == *list)
+			return (1);
+	}
+	return (0);
+}
