218da24e1a8094f496748d5d0cb20bcde670660d
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-06 16:39:03 +0100

    error handling c

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
         with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print(f"Error code: (e.code)")
+        print("Error code: {}".format(e.code))
