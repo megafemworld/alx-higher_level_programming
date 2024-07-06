0105cbb6ee6240981bf58825d19fd8b748d989cf
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-06 13:02:56 +0100

    fetcher url correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     url = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(url) as response:
         body = response.read()
-        print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-        print("\t- utf8 cpntent:", body.decode('utf-8'))
+        print("Body response: ")
+        print("\t- type: ".format(type(body)))
+        print("\t- content: ".format(body))
+        print("\t- utf8 cpntent: ".format(body.decode('utf-8')))
