c3984d6f2754ddaeb6101080a5e402ee25bcde42
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-06 23:05:30 +0100

    json loading json n

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ script that takes in a letter and
-sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter."""
+sends a POST request to http://0.0.0.0:5000/search_user with
+the letter as a parameter."""
 
 import requests
 import sys
