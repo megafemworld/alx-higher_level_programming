09091efef16fa73470ed90ec1128cbd645b83526
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-06 17:31:56 +0100

    requests header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" script that takes in a URL,
+sends a request to the URL and displays the 
+value of the variable X-Request-Id"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    resp = requests.get(url)
+    x_r_id = resp.headers.get('X-Request-Id')
+    print(x_r_id)
