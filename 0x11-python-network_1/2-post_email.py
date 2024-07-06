dbaee21664a67aded3f71b441db56b037eec4d99
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-06 15:08:45 +0100

    post request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(email).encode()
+    data = urllib.parse.urlencode(email).encode('asii')
     request = url.request.Request(url, data=data, method='POST')
     with urllib.request.urlopen(request) as response:
         print(response.read().decode('utf-8'))
