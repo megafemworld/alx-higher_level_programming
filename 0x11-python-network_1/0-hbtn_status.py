2b427bcb78ec4dda8493d7c0a0f7d201b8e988b8
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-06 12:32:57 +0100

    fetcher url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,11 @@
 """ fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-print("Body response:")
-print("\t- type:", type(body))
-print("\t- content:", body)
-print("\t- utf8 cpntent:", body.decode('utf-8'))
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 cpntent:", body.decode('utf-8'))
