bfacae8cae5be6505aa5c55b468c7e54f3d057e6
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-07 21:45:12 +0100

    github api commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""Print all commits by: `<sha>: <author name>` (one by line)"""
-
-import requests
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
 import sys
+import requests
 
-repo = sys.argv[1]
-owner = sys.argv[2]
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(repo, owner)
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
     r = requests.get(url)
     commits = r.json()
     try:
         for i in range(10):
             print("{}: {}".format(
-                    commits[i].get("sha"),
-                    commits[i].get("commit").get("author").get("name")))
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
