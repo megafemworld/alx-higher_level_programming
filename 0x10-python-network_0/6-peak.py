3d8be9a216afa6d96ce4338cb18270d6b5167dd3
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-19 16:12:35 +0100

    status code

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function that finds a peak in a list of unsorted integers."""
+
+def find_peak(list_of_integers):
+    """find the peak of a list"""
+    if (len(list_of_integer)) < 3:
+        return None
