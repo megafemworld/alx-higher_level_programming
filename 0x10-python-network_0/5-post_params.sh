52dd1c66f1fd00002df6bb971bdc42926b90d219
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-19 13:14:55 +0100

    post send data 3 lines

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # sends a POST request to the passed URL, and displays the body of the response
-
-url="$1"
-EMAIL="test@gmail.com"
-SUBJECT="I will always be here for PLD"
-curl -s -X POST -d "email=${EMAIL}&subject=${SUBJECT}" "$url"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
