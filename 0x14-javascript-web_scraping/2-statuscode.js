ba1bbe3e8c7b7456b32da50cf3e281cb514c8b2d
Femi Mehalayese <megafemgroup@gmail.com>
2024-07-31 12:25:22 +0100

    statuscode 1

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ request(url, function (err, response) {
   if (err) {
     console.log(err);
   } else {
-    console.log('code: ' + response.statuscode);
+    console.log('code: ' + response.statusCode);
   }
 });
