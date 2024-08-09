69c857849ab8127a917bebed3cd56b2f3a5a0e2e
Femi Mehalayese <megafemgroup@gmail.com>
2024-08-09 14:18:06 +0100

    2nd biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const list = process.argv.slice(2);
-  const list_num = list.map(num => parseInt(num, 10));
-  list_num.sort((a, b) => b - a);
-  console.log(list_num[1]);
+  const listnum = list.map(num => parseInt(num, 10));
+  listnum.sort((a, b) => b - a);
+  console.log(listnum[1]);
 }
