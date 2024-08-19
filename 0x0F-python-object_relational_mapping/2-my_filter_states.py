ceabd9236f20f1a2eb90347a7ba9ab754b8c87e1
Femi Mehalayese <megafemgroup@gmail.com>
2024-08-19 17:46:14 +0100

    match input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N (upper N)"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states \
+            WHERE BINARY name = '{}' ORDER BY id ASC".format(argv[4])
+    )
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
