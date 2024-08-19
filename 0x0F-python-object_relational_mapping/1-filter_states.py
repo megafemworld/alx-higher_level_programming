f29104eb9ae5e0d731aad9a3cffe93af38327cb1
Femi Mehalayese <megafemgroup@gmail.com>
2024-08-19 16:21:31 +0100

    like

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""lists all states with a name starting with N (upper N)"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
