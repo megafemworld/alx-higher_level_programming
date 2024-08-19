ca3783e3f0abdd89e7a51214bc22eed552be50b8
Femi Mehalayese <megafemgroup@gmail.com>
2024-08-19 15:37:38 +0100

    seleect by state id asc

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port="3306",
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    rows = cur.execute("SELECT * FROM states ORDER BY states.id")
-    for i in range(rows):
-        print(cur.fetchone())
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
