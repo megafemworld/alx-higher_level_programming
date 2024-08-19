2c355c23e81da3abc5e06ae6e287f6cc01f309fb
Femi Mehalayese <megafemgroup@gmail.com>
2024-08-19 17:47:35 +0100

    match input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
-    cursor.execute(
+    c.execute(
         "SELECT * FROM states \
             WHERE BINARY name = '{}' ORDER BY id ASC".format(argv[4])
     )
