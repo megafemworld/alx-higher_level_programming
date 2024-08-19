9a7ea179f160e03ae7c11f6455111eaea6b98006
Femi Mehalayese <megafemgroup@gmail.com>
2024-08-19 16:24:29 +0100

    like case sensentive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    c.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
     rows = c.fetchall()
     for row in rows:
         print(row)
