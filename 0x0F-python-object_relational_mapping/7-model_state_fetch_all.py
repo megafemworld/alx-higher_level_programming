917e15a4f2b65571eb8c2c41d9cda64f1add4c32
Femi Mehalayese <megafemgroup@gmail.com>
2024-08-20 18:23:27 +0100

    model state detch all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,7 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format
-                            (argv[1], argv[2], argv[3]))
+                           (argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
