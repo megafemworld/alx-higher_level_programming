acdb9dfccc5abb44bc9fdd94c97d09ca5d7611c0
Femi Mehalayese <megafemgroup@gmail.com>
2024-08-20 10:25:33 +0100

    corrected

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
     query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
-    c.execute(query, argv[4])
+    c.execute(query, (argv[4],))
     rows = c.fetchall()
     for row in rows:
         print(row)
